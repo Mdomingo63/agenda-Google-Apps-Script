773673cbb1bcfbe490d597b6311fe20963565129
Domingo Sanchez <smatos63@gmail.com>
2022-09-29 10:30:20 -0400

    editar archivo js

--- funciones.js
+++ funciones.js
@@ -7,3 +7,12 @@ function doGet(){
 
        return HtmlService.createHtmlOutputFromFile(nombre).getContent();
     }
+
+       function obtenerContactos(){
+             
+            let hoja = SpreadsheetApp.openById("1FY5x3sEIB0aKtt2jkaiAjowL_kIYqGiUU8lfj5TPtWo").getActiveSheet();
+
+            let datos = hoja.getDataRange().getValues();
+            return datos;
+
+       }
